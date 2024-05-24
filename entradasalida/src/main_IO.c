667ca8660adc7ac8178046fd8150a09be18fd336
santgomez <santgomez@frba.utn.edu.ar>
2024-05-24 02:18:37 +0000

    Creo que fin de interfaz generica IO

--- entradasalida/src/main_IO.c
+++ entradasalida/src/main_IO.c
@@ -7,51 +7,50 @@
 #include <../include/IO.h>
 #include <../include/inicializar_io.h>
 
-
-void EjecutarInterfazGenerica(char* nombre,t_config* config_interface){
-	char* TIPO_INTERFAZZ = config_get_string_value(config_interface, "TIPO_INTERFAZ");
+void ejecutarInterfazGenerica(char* nombre,t_config* config_interface){
+	log_info(io_logger,"Iniciando interfaz Generica"); 
 	int TIEMPO_UNIDAD_TRABAJOO = config_get_int_value(config_interface, "TIEMPO_UNIDAD_TRABAJO"); //por default es 250, osea, 4 unidades de tiempo equivalen a un 1 segundo
+	int Unidades_De_Trabajo = 4;
 
-	log_info(io_logger,"Noto que estas intentando crear un interfaz del tipo %s",TIPO_INTERFAZZ); //deberia decir del tipo GENERICA
+	log_info(io_logger,"Iniciando sleep");
+	usleep(Unidades_De_Trabajo*(TIEMPO_UNIDAD_TRABAJOO*1000));//esta funcion anda con microsegundos, por eso se multiplican por 1000, asi cada unidad de trabajo equivale a un milisegundo
+	log_info(io_logger,"Fin de sleep");
 
-	log_info(io_logger,"Buenas noches es hora de dormirse");
-	//sleep(20);//Aca se podria usar usar la variable TIEMPO_UNIDAD_TRABAJOO de alguna forma
-	usleep(TIEMPO_UNIDAD_TRABAJOO*1000);//esta funcion anda con microsegundo, por eso se multiplican por 1000, asi cada unidad de trabajo equivale a un milisegundo
-	log_info(io_logger,"Ya desperte del sueño");
+	log_trace(io_log_debug, "Fin de Interfaz Generica");
 
 
 }
+
+void iniciarInterfaz(char* nombre_Interface,t_config* config_interface){
+	log_info(io_logger,"Incializando interfaz"); 
+	char* TIPO_INTERFAZZ = config_get_string_value(config_interface, "TIPO_INTERFAZ");
+	if (strcmp(TIPO_INTERFAZZ, "GENERICA")==0) {
+		log_info(io_logger,"La interfaz a iniciar es del tipo Generica"); 
+		ejecutarInterfazGenerica(nombre_Interface, config_interface);
+	}else if (strcmp(TIPO_INTERFAZZ, "STDIN")==0){
+		log_info(io_logger,"La interfaz a iniciar es del tipo STDIN"); 
+		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
+	}else if (strcmp(TIPO_INTERFAZZ, "STDOUT")==0){
+		log_info(io_logger,"La interfaz a iniciar es del tipo STDOUT"); 
+		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
+	}else if (strcmp(TIPO_INTERFAZZ, "DialFS")==0){
+		log_info(io_logger,"La interfaz a iniciar es del tipo DialFS"); 
+		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
+	}else{
+		log_error(io_log_debug, "No se pudo identificar correctamente el tipo de interfaz");
+	}
+
+}
+
+
 
 int main(){
 
 	inicializar_io();
 
-	log_info(io_logger,"Hora del rock! Decime que tipo de interfaz inicializa el I/O");
-
-	int interfazACrear = 1; //cada numero vendria a representar una interfaz generica, esto me parece que conviene cambiarlo a futuro, tal vez un enum o algo
-
-	switch (interfazACrear) {
-		case 1:
-			EjecutarInterfazGenerica("interfaz1",io_config);
-			break;
-		case 2:
-            log_info(io_logger,"Por cual motivo pondrias un 2 aca? XD");
-			break;
-		case -1:
-			log_trace(io_logger, "Mepa que ocurrio un bug o algo");
-			break;
-		default:
-			log_warning(logger,"PARA EMOCION!!! esa interfaz todavia no la implemente");
-			break;
-		}
-
-	log_info(io_logger,"Acabo de salir del switch");
-
-
 	
-	/*
 	fd_memoria = iniciar_conexion(PUERTO_MEMORIA, "MEMORIA",io_log_debug);
-
+	
 	fd_kernel = iniciar_conexion(PUERTO_KERNEL, "KERNEL",io_log_debug);
 
 
@@ -62,7 +61,17 @@ int main(){
 	pthread_t hilo_memoria;
 	pthread_create(&hilo_memoria, NULL, (void*)atender_io_memoria, NULL);
 	pthread_join(hilo_memoria, NULL);
-	*/
+	
+
+	log_info(io_logger,"Eligiendo interfaz a iniciar");
+
+	char* nombreInterACrear = "int1";
+
+	iniciarInterfaz(nombreInterACrear, io_config);
+
+
+
+	log_info(io_logger,"Fin de Entrada/Salida");
 	
 	return 0;
 }
