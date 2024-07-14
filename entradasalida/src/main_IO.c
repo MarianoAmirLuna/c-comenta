9a18eab48ef6f1007d938f9ff5a0258f64f1041a
santgomez <santgomez@frba.utn.edu.ar>
2024-07-14 00:27:30 +0000

    agrego base stdin y stdout

--- entradasalida/src/main_IO.c
+++ entradasalida/src/main_IO.c
@@ -6,6 +6,22 @@
 #include <commons/log.h>
 #include <../include/IO.h>
 #include <../include/inicializar_io.h>
+#include <readline/readline.h>
+#include <string.h>
+
+t_config*  crearConfig(char* direccion){
+
+	t_config* configDevolver = config_create(direccion); //esto te pide la ruta del config
+
+	if ( configDevolver == NULL)	
+	{
+		perror("Hay un error al iniciar el config.");
+		exit(EXIT_FAILURE);
+	}
+
+	return configDevolver;
+}
+
 
 void ejecutarInterfazGenerica(char* nombre,t_config* config_interface){
 	log_info(io_logger,"Iniciando interfaz Generica"); 
@@ -21,74 +37,134 @@ void ejecutarInterfazGenerica(char* nombre,t_config* config_interface){
 
 }
 
-void iniciarInterfaz(char* nombre_Interface,t_config* config_interface){
+void ejecutarInterfazSTDIN(char* nombre,t_config* config_interface){
+	log_info(io_logger,"Iniciando interfaz STDIN");
+
+		char*  textoAEscribir;
+		log_info(io_logger,"Escriba el texto que desea guardar");
+		textoAEscribir = readline(">");
+
+	log_info(io_logger,"El texto a guardar es %s",textoAEscribir);
+	
+	free(textoAEscribir);
+}
+
+
+void ejecutarInterfazSTDOUT(char* nombre,t_config* config_interface){
+
+	log_info(io_logger,"Iniciando interfaz STDOUT");
+
+	char* textoLeido;
+
+	textoLeido="Texto de prueba";
+	
+	log_info(io_logger,"El texto leido es %s",textoLeido);
+
+}
+
+
+
+void iniciarInterfaz(char* nombre_Interface,char* direccion_Config){
+
 	log_info(io_logger,"Incializando interfaz"); 
+
+	t_config* config_interface = crearConfig(direccion_Config);
 
 	char* TIPO_INTERFAZZ = config_get_string_value(config_interface, "TIPO_INTERFAZ");
 
 	if (strcmp(TIPO_INTERFAZZ, "GENERICA")==0) {
-
 		log_info(io_logger,"La interfaz a iniciar es del tipo Generica"); 
 		ejecutarInterfazGenerica(nombre_Interface, config_interface);
-
 	}else if (strcmp(TIPO_INTERFAZZ, "STDIN")==0){
-
 		log_info(io_logger,"La interfaz a iniciar es del tipo STDIN"); 
-		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
-
+		ejecutarInterfazSTDIN(nombre_Interface, config_interface);
 	}else if (strcmp(TIPO_INTERFAZZ, "STDOUT")==0){
-
-		log_info(io_logger,"La interfaz a iniciar es del tipo STDOUT"); 
-		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
-
+		log_info(io_logger,"La interfaz a iniciar es del tipo STDOUT");  
+		ejecutarInterfazSTDOUT(nombre_Interface, config_interface);
 	}else if (strcmp(TIPO_INTERFAZZ, "DialFS")==0){
-
 		log_info(io_logger,"La interfaz a iniciar es del tipo DialFS"); 
 		log_error(io_log_debug,"Esta interfaz no se encuentra implementada");
-
 	}else{
-
 		log_error(io_log_debug, "No se pudo identificar correctamente el tipo de interfaz");
-
 	}
 
 }
 
+
+
 int main(){
 
-	
 	inicializar_io();
 
-	fd_memoria = iniciar_conexion(PUERTO_MEMORIA, "MEMORIA",io_log_debug);
 
+	log_info(io_logger,"Inicializando Entrada/Salida");
+
+
+	
+	fd_memoria = iniciar_conexion(PUERTO_MEMORIA, "MEMORIA",io_log_debug);
+	
 	fd_kernel = iniciar_conexion(PUERTO_KERNEL, "KERNEL",io_log_debug);
 
-	pthread_t hilo_memoria;
-	pthread_create(&hilo_memoria, NULL, (void*)atender_io_memoria, NULL);
-	pthread_join(hilo_memoria, NULL);
 
 	pthread_t hilo_kernel;
 	pthread_create(&hilo_kernel, NULL, (void*)atender_io_kernel, NULL);
 	pthread_detach(hilo_kernel);
 
-	log_info(io_logger,"Eligiendo interfaz a iniciar");
+	pthread_t hilo_memoria;
+	pthread_create(&hilo_memoria, NULL, (void*)atender_io_memoria, NULL);
+	pthread_join(hilo_memoria, NULL);
+	
 
-	char* nombreInterACrear = "int1";
 
-	iniciarInterfaz(nombreInterACrear, io_config);
+	char* nombreInterACrear;
 
+	do{
+
+
+		log_info(io_logger,"Escriba el nombre de la interfaz");
+		nombreInterACrear = readline(">");
+
+		if(string_is_empty(nombreInterACrear)){
+			log_warning(io_log_debug,"Una interfaz no puede tener el nombre vacio");
+		}
+	}while(string_is_empty(nombreInterACrear));
+	
+	log_info(io_logger,"El nombre elegido es %s",nombreInterACrear);
+
+
+	char* direccionConfigInterCrear;
+
+	do{
+
+		log_info(io_logger,"Escriba la direccion del archivo de configuracion");
+		direccionConfigInterCrear = readline(">");
+
+		if(string_is_empty(direccionConfigInterCrear)){
+			log_warning(io_log_debug,"Una direccion no puede ser vacia");
+		}
+
+	}while(string_is_empty(direccionConfigInterCrear));
+
+	log_info(io_logger,"La direccion elegida es %s",nombreInterACrear); //TODO: ¿Se deberia considerar la posibilidad de que en esa direccion no haya archivo de configuracion?
+	
+	free(direccionConfigInterCrear);
+
+	direccionConfigInterCrear="/home/utnso/Desktop/ClonOperativos/tp-2024-1c-Granizado/entradasalida/entradasalida.config"; //TODO: borrar este hardcodeo de la direccion del config
+	
+	iniciarInterfaz(direccionConfigInterCrear, direccionConfigInterCrear);
+	
+	//free(direccionConfigInterCrear);
+	free(nombreInterACrear);
 
 
 	log_info(io_logger,"Fin de Entrada/Salida");
 	
 	return 0;
-	
 }
 
 //iniciar servidor => retorna socket servidor
 //esperar cliente => retorna socket cliente
 //iniciar conexion / crear conexion => retorna socket cliente
-
 
 
 
